3a5947bfd387ddc811cc5b6edb198f532b8a0a00
Kurulucifer <28943130+Kurulucifer@users.noreply.github.com>
2026-06-21 23:34:15 -0700

    Fix variable name issue in task-maker.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import "./stylesheet.css";
+import taskMaker from './task-maker.js';
+import taskChecklistMaker from "./task-checklist-maker.js";
+
+const name = "buy milk";
+const date = new Date();
+const details = "gotta buy milk from ralphs";
+const notes = "remember to bring the coupon";
+const priority = "3";
+
+const task1 = taskMaker(name, date, details, notes, priority);
+console.log(task1);
+console.log(task1.getField("name"));
+task1.updateField({name : "buy yogurt", details : "gotta buy yogurt from ralphs"});
+console.log(task1.getAllFields());
--- src/task-maker.js
+++ src/task-maker.js
@@ -1,3 +1,30 @@
-function taskMaker(name, dueDate, details, notes, priority) {
+// all strings except initDate, which is a Date object, probably?
+// not sure what the priority levels should be yet
+function taskMaker(initName, initDate, initDetails, initNotes, initPriority) {
+    let task = {
+        name: initName,
+        date: initDate,
+        details: initDetails,
+        notes: initNotes, 
+        priority: initPriority,
+    };
+
+    const getField = (field) => task[field];
+
+    const getAllFields = () => ( { ...task} );
+
+    // thank you claude for teaching me the versatility of spread syntax
+    // however I will need to format the input as objects in DOM module
+    const updateField = (fields) => {
+        task = {...task, ...fields}
+    };
     
-}
+    return {
+        getField,
+        updateField,
+        getAllFields,
+    };
+};
+
+export default taskMaker;
+
